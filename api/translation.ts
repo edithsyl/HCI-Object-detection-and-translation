c3f192e59173e99317a2d8e1b28fb212c5d3d8fe
syleung <syleung1220@gmail.com>
2024-05-01 01:49:52 +0800

    update translation, CORs network err in progress

--- api/translation.ts
+++ api/translation.ts
@@ -1,28 +1,32 @@
 const axios = require('axios');
-/**
- * https://cloud.google.com/translate/docs/reference/rest/v3beta1/projects/translateText
- */
-export async function getTranslation(text: String): Promise<String>{
+import { OPENAI_API_KEY, OPENAI_ENDPOINT } from '../config';
+
+
+export async function getTranslation(text: string, targetLang: string): Promise<string>{
     try {
+            console.log('getTranslation run')
+            const prompt = `Please translate the following word or sentences into ${targetLang}: ${text}. Provide the answer only. Remove the '.' at the end. If you could not provide a translation due to inappropriate content or other reasons, please return 'N/A'.`
             const reqBody = {
                     messages:[
-                        {role: "system", content: "You are a professor."},
-                        {role: "user", content: `Please translate the following word or sentences: ${text}`}
+                        {role: "system", content: "You are a translating machine."},
+                        {role: "user", content: prompt}
                     ]
                 };
             const response = await axios.post(
-                'https://hkust.azure-api.net/openai/deployments/gpt-35-turbo/chat/completions?api-version=2023-05-15', 
+                OPENAI_ENDPOINT, 
                 reqBody,
                 {
                     headers:  {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Headers': '*',
                         'Content-Type': 'application/json',
-                        'api-key': '56120e26c7bb4bc5a55387b76eefc1d6' // openai primary key
+                        'api-key': OPENAI_API_KEY // openai primary key
                     },
                 });
             console.log(response);
-            return 'response';
+            return response.choices[0].message.content;
         } catch (error) {
             console.error(error);
-            return ''
+            return '--'
         }
 }
