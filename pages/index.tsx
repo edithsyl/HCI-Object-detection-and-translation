0668caeb3118a903858810a8f02b0b4638743f4d
syleung <syleung1220@gmail.com>
2024-05-03 10:13:21 +0800

    remove unnecessary component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Yolo from "../components/models/Yolo";
-import VoiceRecognition from "../components/speech_recognition/speech_to_text";
 import dynamic from "next/dynamic";
 import SpeechTranslator from "../components/speech_recognition/SpeechTranslate"
 
@@ -10,7 +9,6 @@ export default function Home() {
       <main className="font-mono flex flex-col justify-center items-center  w-screen">
         <h1 className="m-5 text-xl font-bold">Real-Time Object Detection</h1>
         <Yolo />
-        {/* <VoiceRecognition /> */}
         <SpeechTranslator/>
       </main>
     </>
