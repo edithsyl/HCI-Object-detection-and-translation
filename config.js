94d4ddc3484dc3958ff1cd7b8baef29aa10daba6
syleung <syleung1220@gmail.com>
2024-05-06 10:04:46 +0800

    clean config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// export const OPENAI_API_KEY = 'your-api-key-here';
 export const PUBLIC_EC2_ADD = 'http://34.235.87.16:8888'
 export const HOST_AWS = 'https://bu1ik0gj36.execute-api.us-east-1.amazonaws.com'
-export const ENDPOINT_AWS = process.env.NEXT_PUBLIC_ENDPOINT_AWS || 'https://bu1ik0gj36.execute-api.us-east-1.amazonaws.com/openai-dev/myHttpProxy'
-export const ACCESS_KEY_AWS = process.env.NEXT_PUBLIC_ACCESS_KEY_AWS ||'null-or-undefined-secret-key'
+export const ENDPOINT_AWS = process.env.NEXT_PUBLIC_ENDPOINT_AWS || 'null-or-undefined-endpoint'
+export const ACCESS_KEY_AWS = process.env.NEXT_PUBLIC_ACCESS_KEY_AWS ||'null-or-undefined-access-key'
 export const SECRET_KEY_AWS = process.env.NEXT_PUBLIC_SECRET_KEY_AWS || 'null-or-undefined-secret-key'
-export const SESSION_TOKEN_AWS = process.env.NEXT_PUBLIC_SESSION_TOKEN_AWS || 'null-or-undefined-secret-key'
+export const SESSION_TOKEN_AWS = process.env.NEXT_PUBLIC_SESSION_TOKEN_AWS || 'null-or-undefined-session-token'
